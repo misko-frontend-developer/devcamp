57c5d8c4b690a543d214fbb8579eb10423721767
milos <milos.marjanovic159@gmail.com>
2025-03-18 11:59:26 +0100

    database setup

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,21 +1,48 @@
+const Bootcamp = require("../models/Bootcamp");
+
 //@desc Get all bootcamps
 //@route GET /api/v1/bootcamps
 //@access Public
-exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, msg: "Show all" });
+exports.getBootcamps = async (req, res, next) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+    res.status(200).json({ success: true, data: bootcamps });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 };
 
 //@desc Get single bootcamp
 //@route GET /api/v1/bootcamps/:id
 //@access Public
-exports.getBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: "Show all" });
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 };
 
 //@desc Create bootcamp
 //@route POST /api/v1/bootcamps
 //@access Private
-exports.createBootcamp = (req, res, next) => {};
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+
+    if (!bootcamp) {
+      return res.status(400).json({ success: false });
+    }
+    res.status(201).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ success: false });
+  }
+};
 
 //@desc Update bootcamp
 //@route PUT /api/v1/bootcamps
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const connectDB = require("./config/db");
+
 dotenv.config({ path: "./config/config.env" });
+
+connectDB();
 
 const bootcamps = require("./routes/bootcamps");
 const logger = require("./middleware/logger");
 const app = express();
+
+app.use(express.json());
 
 app.use(logger);
 
@@ -16,9 +22,14 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(
     `Server running on ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
   )
 );
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error ${err.message}`);
+  server.close(() => process.exit(1));
+});
