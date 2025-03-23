3b10b9d27ece2b7e75d85cf3cedbd37143c53412
milos <milos.marjanovic159@gmail.com>
2025-03-23 12:39:38 +0100

    error handlers added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const connectDB = require("./config/db");
+const errorHandler = require("./middleware/error");
 
 dotenv.config({ path: "./config/config.env" });
 
@@ -19,6 +20,7 @@ if (process.env.NODE_ENV === "developement") {
   app.use(morgan("dev"));
 }
 app.use("/api/v1/bootcamps", bootcamps);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
